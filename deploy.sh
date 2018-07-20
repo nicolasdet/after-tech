f54edc97d71336519698efa67419b8eb313b579f
Unknown <n.tiesenhausen@gmail.com>
2018-07-20 12:54:04 +0200

    rollback

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # GNU bash, version 4.3.46
 echo "clef  ${key}";
-sshpass -p ${key} ssh root@vps554131.ovh.net bash after-tech/script.sh;
+sshpass -p ${key} ssh -o StrictHostKeyChecking=no root@vps554131.ovh.net bash after-tech/script.sh;
