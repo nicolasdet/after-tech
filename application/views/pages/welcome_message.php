a12bcdea84a07c6ec7a7d554abe2f8420f44a0d0
Unknown <n.tiesenhausen@gmail.com>
2018-07-22 14:40:56 +0200

    home page Text + image + loren ipsum

--- application/views/pages/welcome_message.php
+++ application/views/pages/welcome_message.php
@@ -1,16 +1,16 @@
 
             <section class="cover height-80 imagebg text-center" data-overlay="3">
                 <div class="background-image-holder">
-                    <img alt="background" src="public/assets/img/cowork-7.jpg" />
+                    <img alt="background" src="public/assets/img/afterWorkHands.jpg" />
                 </div>
                 <div class="container pos-vertical-center">
                     <div class="row">
                         <div class="col-md-10">
                             <h1>
-                                Work alongside freelancers, makers and doers.
+                                Crée ou rejoignez des after-tech dès maintenent.
                             </h1>
                             <p class="lead">
-                                Stack offers a clean and contemporary look to suit a range of purposes from corporate, tech startup, marketing site to digital storefront.
+                                
                             </p>
                             <!--end of modal instance-->
                         </div>
@@ -23,9 +23,9 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-lg-8">
-                            <h2>Coworking in the heart of historic Carlton.</h2>
+                            <h2>Des after work organiser spécialement pour vous et vos collegues !</h2>
                             <p class="lead">
-                                Stack is a collective of like-minded creatives working together in a converted church in the heart of Fitzroy North. Featuring multiple private and shared spaces with all the conveniences of a modern creative envrionment, you'll love working at Stack.
+                                Organisez des évenements entres collegues, inter-entreprise afin de dynamisée vos soirée et découvrir de nouvelle personnes.
                             </p>
                         </div>
                     </div>
@@ -38,25 +38,25 @@
                     <div class="gallery-1 row">
                         <div class="col-md-6">
                             <div class="gallery__image">
-                                <a href="public/assets/img/cowork-2.jpg" data-lightbox="Gallery 1">
+                                <a href="public/assets/img/img-atferWork-1.jpg" data-lightbox="Gallery 1">
                                     <div class="background-image-holder">
-                                        <img alt="Image" src="public/assets/img/cowork-2.jpg" />
+                                        <img alt="Image" src="public/assets/img/img-atferWork-1.jpg" />
                                     </div>
                                 </a>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="gallery__image">
-                                <a href="public/assets/img/cowork-6.jpg" data-lightbox="Gallery 1">
+                                <a href="public/assets/img/meeting-bar.jpg" data-lightbox="Gallery 1">
                                     <div class="background-image-holder">
-                                        <img alt="Image" src="public/assets/img/cowork-6.jpg" />
+                                        <img alt="Image" src="public/assets/img/meeting-bar.jpg" />
                                     </div>
                                 </a>
                             </div>
                             <div class="gallery__image">
-                                <a href="public/assets/img/knowledge-1.jpg" data-lightbox="Gallery 1">
+                                <a href="public/assets/img/afterWorkPro.jpg" data-lightbox="Gallery 1">
                                     <div class="background-image-holder">
-                                        <img alt="Image" src="public/assets/img/knowledge-1.jpg" />
+                                        <img alt="Image" src="public/assets/img/afterWorkPro.jpg" />
                                     </div>
                                 </a>
                             </div>
@@ -70,9 +70,9 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-lg-8">
-                            <h2>What makes Stack different?</h2>
+                            <h2>Quel difference avec un site d'evenement ou un after work classique ?</h2>
                             <p class="lead">
-                                We do things a little differently, here's a quick overview of just a few of Stack Coworking's unqiue offering.
+                                loren ipsum
                             </p>
                         </div>
                     </div>
@@ -86,9 +86,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Bicycle"></i>
-                                <h4>Central Location</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Stack comes with integration for Mail Chimp and Campaign Monitor forms - ideal for modern marketing campaigns
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -99,9 +99,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Coffee-Machine"></i>
-                                <h4>Premium Facilities</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Combine blocks from a range of categories to build pages that are rich in visual style and interactivity
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -112,9 +112,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Laptop-3"></i>
-                                <h4>Device Lab</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Including the premium Icons Mind icon kit, Stack features a highly diverse set of icons suitable for all purposes.
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -126,9 +126,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Phone-Wifi"></i>
-                                <h4>Superfast Internet</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Stack comes with integration for Mail Chimp and Campaign Monitor forms - ideal for modern marketing campaigns
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -139,9 +139,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Business-ManWoman"></i>
-                                <h4>Meeting Spaces</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Combine blocks from a range of categories to build pages that are rich in visual style and interactivity
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -152,9 +152,9 @@
                         <div class="col-md-4">
                             <div class="feature feature-3 boxed boxed--lg boxed--border text-center">
                                 <i class="icon icon--lg icon-Beer-Glass"></i>
-                                <h4>Regular Events</h4>
+                                <h4>loren ipsum</h4>
                                 <p>
-                                    Including the premium Icons Mind icon kit, Stack features a highly diverse set of icons suitable for all purposes.
+                                    loren ipsum
                                 </p>
                                 <a href="#">
                                     Learn More
@@ -176,13 +176,14 @@
                         <div class="col-md-7 col-lg-5">
                             <div class="boxed boxed--lg border--round bg--white">
                                 <div class="col-lg-10 col-md-12 mx-auto">
-                                    <h3>Coworker Spotlight</h3>
+                                    <h3>Contribuez</h3>
                                     <p class="lead">
-                                        &rdquo;I started working at the Stack coworking space earlier this year and in that time I've met so many inspiring people, and made friends too. I originally joined as an office away from home but have since made Stack my permanent workplace.&ldquo;
+                                        Ce projet est Open-source et tout le monde est inviter à contribuer au projet. 
+                                        Venez en savoir plus sur les pages FAQ et contributing !
                                     </p>
                                     <hr class="short" />
                                     <p>
-                                        Luke launched his startup Vent with people he met while working at Stack Coworking.
+                                        <a> contributing </a>
                                     </p>
                                 </div>
                             </div>
@@ -202,37 +203,37 @@
                         </div>
                         <div class="col-lg-4">
                             <div class="text-block">
-                                <h5>How do refunds work?</h5>
+                                <h5>loren ipsum</h5>
                                 <p>
-                                    Open a
-                                    <a href="#">refund request</a> with us and we can work with you to resolve it ASAP.
+                                   loren ipsum
+                                    <a href="#">loren ipsum</a> loren ipsum
                                 </p>
                             </div>
                             <div class="text-block">
-                                <h5>Can I pay using AMEX?</h5>
+                                <h5>loren ipsum</h5>
                                 <p>
-                                    Yes, we accept all major credit cards, including AMEX, so rack up those points!
+                                    loren ipsum
                                 </p>
                             </div>
                             <div class="text-block">
-                                <h5>Is there a bulk-buy discount?</h5>
+                                <h5>loren ipsum</h5>
                                 <p>
-                                    We have corporate and enterprise arrangements that our pricing team can assist with on a case-by-case basis.
-                                    <a href="#">Contact Us</a> for info.
+                                   loren ipsum
+                                    <a href="#">loren ipsum</a> loren ipsum
                                 </p>
                             </div>
                         </div>
                         <div class="col-lg-4">
                             <div class="text-block">
-                                <h5>Is there a minimum contract term?</h5>
+                                <h5>loren ipsum</h5>
                                 <p>
-                                    No! The beauty of our service is that you can cancel anytime you need to &mdash; no questions asked.
+                                    loren ipsum
                                 </p>
                             </div>
                             <div class="text-block">
-                                <h5>Do I need an SSL certificate?</h5>
+                                <h5>loren ipsum</h5>
                                 <p>
-                                    This depends on whether your plan to process the payment on your site or not. We recommend using a third-party provider to unburden yourself.
+                                   loren ipsum
                                 </p>
                             </div>
                         </div>
